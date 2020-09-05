d84f47b098af50c6b923c47e879c70e705a27048
Or Lavy <orlavyil@gmail.com>
2020-09-05 14:33:39 +0200

    Adds theme

--- src/AppWrapper.tsx
+++ src/AppWrapper.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React from "react";
 import App from "./App";
+import { CssBaseline } from "@material-ui/core";
+import {
+  makeStyles,
+  StylesProvider,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import { baseTheme } from "./theme/Theme";
 
-interface IProps {
+interface IProps {}
 
-}
-
-export const AppWrapper = React.memo<IProps>(props => {
-   return <App/>
+export const AppWrapper = React.memo<IProps>((props) => {
+  return (
+    <StylesProvider injectFirst>
+      <ThemeProvider theme={baseTheme}>
+        <App /> <CssBaseline />
+      </ThemeProvider>
+    </StylesProvider>
+  );
 });
